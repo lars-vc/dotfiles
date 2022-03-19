768489419898810482fad8ada84e961298423078
Lars Van Cauter <57136208+lars-vc@users.noreply.github.com>
2022-03-19 20:08:15 +0100

    Update first_install.sh

--- first_install.sh
+++ first_install.sh
@@ -2,8 +2,9 @@
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
 sudo apt update
-sudo apt install git-all
-sudo apt install gh
+sudo apt install git-all -y
+sudo apt install gh -y
+sudo apt upgrade -y
 gh auth login
 cd /
 sudo sh -c "$(curl -fsLS chezmoi.io/get)"
