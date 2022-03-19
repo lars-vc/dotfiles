66ca6761622055ce6a815f30c0617ea4360d75d2
Lars Van Cauter <larsvancauter@gmail.com>
2022-03-19 17:39:29 +0100

    updated install script

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
-
+sudo apt update
 # NeoVim
-sudo add-apt-repository ppa:neovim-ppa/stable
+sudo add-apt-repository ppa:neovim-ppa/stable -y
 # gh github cli
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-
+# Ranger
+sudo apt install ranger -y
+# UPDATE
 sudo apt update
 sudo apt upgrade
-# NeoVim dependencies
+
+# General
+# extracting stuff
+sudo apt install dtrx -y
+# python is python3
+sudo apt install python-is-python3 -y
+# gh github client
 sudo apt install gh
-sudo apt install npm
+
+# NeoVim dependencies
+sudo apt install npm python3 python3-pip exuberant-ctags ripgrep wdutch sqlite3 libsqlite3-dev -y
 sudo npm install -g yarn
 sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
-sudo apt install python3
-sudo apt install python3-pip
-sudo apt install exuberant-ctags
-sudo apt install sqlite3 libsqlite3-dev
-pip install ropevim
-sudo apt install ripgrep
-sudo apt install wdutch
+sudo pip install ropevim
 
+# Ranger
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
+sudo pip install ueberzug
+sudo apt install libjpeg8-dev zlib1g-dev libxtst-dev ffmpegthumbnailer -y
+
+# UPDATE
 sudo apt update
 sudo apt upgrade
