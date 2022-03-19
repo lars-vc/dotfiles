fb2443437c23cf5903fa34acd6e4be3c703eeed1
Lars Van Cauter <57136208+lars-vc@users.noreply.github.com>
2022-03-19 19:48:55 +0100

    Create first_install.sh

---
+++ first_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install git-all
+sudo apt install gh
+gh auth login
+sudo apt install snapd
+sudo snap install chezmoi --classic
+chezmoi init --apply https://github.com/lars-vc/dotfiles
