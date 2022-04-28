ba150a089866b7185cef6c89c425b1b40d06b3cd
Lars Van Cauter <larsvancauter@gmail.com>
2022-04-28 19:13:22 +0200

    better braces

--- private_dot_config/nvim/lua/lars-vc/autopairs.lua
+++ private_dot_config/nvim/lua/lars-vc/autopairs.lua
@@ -2,9 +2,9 @@ local autopairs = require('nvim-autopairs')
 
 autopairs.setup({
     enable_check_bracket_line = true,
-    ignored_next_char = "[%w%.]", -- will ignore alphanumeric and `.` symbol
+    ignored_next_char = "[%w]", -- will ignore alphanumeric and `.` symbol
     fast_wrap = {
       map = '<M-m>',
-      keys = 'hjklmfdsqazertyuiop',
+      keys = 'jklmfdsqazertyuiop',
     }
 })
