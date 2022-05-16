40df8914ecfa4e83a47f9a0f360630640b0e0075
Lars Van Cauter <larsvancauter@gmail.com>
2022-05-16 20:31:01 +0200

    install software-properties-common

--- first_install.sh
+++ first_install.sh
@@ -9,6 +9,7 @@ if [ -x "$(which apt)" ]; then
     # add gh to apt repo
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt install software-properties-common
 fi
 # sudo apt update
 # sudo apt install git-all -y
