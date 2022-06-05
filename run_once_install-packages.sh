bec6d6a8905f34260676847c532ec5d625627d94
Lars Van Cauter <larsvancauter@gmail.com>
2022-06-05 10:51:13 +0200

    zsh updates

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -32,6 +32,10 @@ $pkgm pkg-config htop tmux sysstat xclip exa bat
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # auto complete
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# syntax highlight
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# zsh theme powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # set zsh as default
 chsh -s $(which zsh)
 
