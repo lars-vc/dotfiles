f6c920673fbcd657572f5f5ed220af8bdfcfc672
Lars Van Cauter <larsvancauter@gmail.com>
2022-07-28 19:06:25 +0200

    keybindings script

--- private_dot_config/i3/i3scripts/executable_keybinds.sh
+++ private_dot_config/i3/i3scripts/executable_keybinds.sh
@@ -1,4 +1,9 @@
-RUN=$(cat ~/.config/i3/config | grep 'bindsym' | grep -v '^\s*#' | sed 's/bindsym / /' | rofi -dmenu -p 'Keybinds' | sed 's/^\s*//' | cut -d' ' -f 2- | sed 's/;/ \&\& i3/g' )
-if [[ ! -z "$RUN" ]]; then
+INDEX=$(cat ~/.config/i3/config | grep '^bindsym' | grep -v '^\s*#' | sed 's/bindsym //' | sed 's/--no-startup-id //' | sed 's/exec //' | sed 's/\$mod/Mod/' | sed 's/Control/Ctrl/'| sed 's/^\([^ ]*\) \(.*\)$/\1: \2/' | rofi -dmenu -p 'Keybinds' -format i )
+# parse out none list input
+if [[ ! -z "$INDEX" ]] && [[ $INDEX -ge 0 ]]; then
+    # rofi starts at 0 but sed does not
+    I=$(echo $INDEX+1 | bc)
+    # match index with actual none cleaned list and then clean it a bit for i3
+    RUN=$(cat ~/.config/i3/config | grep '^bindsym' | grep -v '^\s*#' | sed 's/bindsym / /' | sed "${I}q;d" | sed 's/^\s*//' | cut -d' ' -f 2- | sed 's/;/ \&\& i3/g')
     i3 $RUN
 fi
